77b94100223cbe7d5d8cf7956eb941caecbf733e
Lance-pixel-dot <renierlawrencetuico143@gmail.com>
2024-12-03 13:25:38 +0800

    Create div function
    Created a function that creates div based on the size

--- javascript.js
+++ javascript.js
@@ -1,20 +1,56 @@
 const container = document.querySelector('#container');
 const changeSize = document.querySelector('.change-size');
+const reset = document.querySelector('.reset')
 
-function createDiv(divSize){
+let draw = false;
 
-    changeSize.addEventListener('click', () => {
-        const size = prompt('Enter Size');
-        size <= 100 ? createDiv(size) : alert("Size must be below 100");
-     })
+let divSize = 16;
+let newDivSize;
+let divHolder;
+
+function createDiv(){
+
+    if(newDivSize > 16 || newDivSize < 16){
+        div.remove();
+        divSize = newDivSize;
+    }else{
+        divSize = 16;
+    }
 
     for(let i = 0; i < divSize * divSize; i++){
         let div = document.createElement('div');
         container.appendChild(div);
         div.addEventListener('mouseenter', () => {
+            if(!draw) return;
             div.style.backgroundColor = 'black';
         })
+
+        div.addEventListener('mousedown', () => {
+            div.style.backgroundColor = 'black';
+        })
+
     }
+
 }
 
-createDiv(16);
+changeSize.addEventListener('click', () => {
+    newDivSize = prompt('Enter Size');
+    newDivSize <= 100 ? createDiv() : alert("Size must be below 100");
+    createDiv();
+ });
+
+reset.addEventListener('click', () => {
+    div.style.backgroundColor = 'bisque';
+    div.remove();
+    divSize = 16;
+});
+
+container.addEventListener('mousedown', () => {
+    draw = true;
+});
+
+container.addEventListener('mouseup', () => {
+    draw = false;
+});
+
+createDiv();
